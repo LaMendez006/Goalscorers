337e9598d4b4e2555f09e8704a7db2cf26a349f2
Oswaldo <oswaldodaza14@gmail.com>
2025-12-08 09:21:02 -0500

    Implementados getters y setter en Equipo

--- src/main/java/com/goalscorers/modelos/Equipo.java
+++ src/main/java/com/goalscorers/modelos/Equipo.java
@@ -9,5 +9,19 @@ package com.goalscorers.modelos;
  * @author hijo
  */
 public class Equipo {
+    private String nombre;
+
+    public Equipo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
 }
