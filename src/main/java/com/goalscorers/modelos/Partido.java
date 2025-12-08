2b12d5025a26f88a560479f3a23a3bb5d2e1690d
Oswaldo <oswaldodaza14@gmail.com>
2025-12-08 09:28:03 -0500

    Agregada clase Partido y sus atributos

---
+++ src/main/java/com/goalscorers/modelos/Partido.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.goalscorers.modelos;
+
+/**
+ *
+ * @author hijo
+ */
+public class Partido {
+    private Seleccion equipoLocal;
+    private Seleccion equipoVisitante;
+    private int puntuacionLocal;
+    private int puntuacionVisitante;
+    private String pais;
+    private String ciudad;
+    private String torneo;
+    private String fecha;
+    private boolean esNeutral; 
+}
