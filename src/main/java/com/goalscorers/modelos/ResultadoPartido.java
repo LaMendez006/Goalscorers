2d66ec3f47d775f49a1d023b589826439dcdfda8
Oswaldo <oswaldodaza14@gmail.com>
2025-12-08 09:38:27 -0500

    Implementado enumerador ResultadoPartido

---
+++ src/main/java/com/goalscorers/modelos/ResultadoPartido.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.goalscorers.modelos;
+
+/**
+ *
+ * @author hijo
+ */
+public enum ResultadoPartido {
+    LOCAL,
+    VISITANTE,
+    EMPATE
+}
