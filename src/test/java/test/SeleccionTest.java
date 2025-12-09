dab27f8a4c9b7332346c573df0de35d2c4c56626
GuppyProg1 <166778263+GuppyProg1@users.noreply.github.com>
2025-12-08 20:40:48 -0500

    Crear clase de prueba SeleccionTest.java

---
+++ src/test/java/test/SeleccionTest.java
@@ -0,0 +1,18 @@
+package com.goalscorers.test;
+
+import com.goalscorers.modelos.Seleccion;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SeleccionTest {
+
+    @Test
+    public void testGetSetNombre() {
+        Seleccion s = new Seleccion("Colombia");
+
+        assertEquals("Colombia", s.getNombre());
+
+        s.setNombre("Brasil");
+        assertEquals("Brasil", s.getNombre());
+    }
+}
